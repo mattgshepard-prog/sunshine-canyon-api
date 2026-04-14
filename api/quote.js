e05812cef0e2c00f63bc97b8d9d3ef127902f815
Matt Shepard <mattgshepard@gmail.com>
2026-04-14 11:42:13 -0600

    TEMP: add _debug_money to quote response for price investigation

--- api/quote.js
+++ api/quote.js
@@ -136,6 +136,7 @@ export default async function handler(req, res) {
           currency: money.currency || 'USD',
           hostPayout,
         },
+        _debug_money: money,
       };
     });
 
